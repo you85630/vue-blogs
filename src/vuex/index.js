c0a6163df098683fe4f72a21edeaf32a39bbe57a
You <you85630@foxmail.com>
2018-01-05 09:17:27 +0800

    init

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import api from 'assets/js/api'
-import mutations from './mutations'
+import * as types from './types'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -51,5 +51,29 @@ export default new Vuex.Store({
       })
     }
   },
-  mutations
+  mutations: {
+    // 获取文章
+    [types.GET_BLOGS] (state, res) {
+      state.blogList = res
+      let list = state.blogList
+      let arr = []
+      // 获取标签
+      let like = 0
+      for (let i = 0; i < list.length; i++) {
+        arr.push(list[i].read)
+        // 标签列表
+        state.labelList.names.push({ name: list[i].label })
+      }
+      arr.forEach(e => {
+        let i = parseInt(e)
+        like += i
+      })
+      // 观光者总人数
+      state.likeNow.likes += like
+    },
+    // 获取新闻
+    [types.GET_NEWS] (state, res) {
+      state.newsList.names = res
+    }
+  }
 })
